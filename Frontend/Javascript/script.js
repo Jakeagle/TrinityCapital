3655f5f379edcb026473f9be2c70d73517b9f6fc
Jakeagle <114176194+Jakeagle@users.noreply.github.com>
2023-10-04 12:03:54 -0500

    Update script.js

--- Frontend/Javascript/script.js
+++ Frontend/Javascript/script.js
@@ -15,7 +15,7 @@ const getPrfBTN = document.getElementById('getProfiles');
 const postBTN = document.getElementById('post');
 const input = document.getElementById('input');
 
-export const socket = io('trinitycapitaltestserver-2.azurewebsites.net');
+export const socket = io('https://trinitycapitaltestserver-2.azurewebsites.net');
 
 console.log('User connected:' + socket.id);
 socket.on('checkingAccountUpdate', updatedChecking => {
@@ -47,13 +47,13 @@ socket.on('donationSaving', updatedDonSav => {
 });
 
 /***********************************************************Server Functions**********************************************/
-const testServerProfiles = 'trinitycapitaltestserver-2.azurewebsites.net/profiles';
+const testServerProfiles = 'https://trinitycapitaltestserver-2.azurewebsites.net/profiles';
 
-const loanURL = 'trinitycapitaltestserver-2.azurewebsites.net/loans';
+const loanURL = 'https://trinitycapitaltestserver-2.azurewebsites.net/loans';
 
-const donationURL = 'trinitycapitaltestserver-2.azurewebsites.net/donations';
+const donationURL = 'https://trinitycapitaltestserver-2.azurewebsites.net/donations';
 
-const donationSavingsURL = 'trinitycapitaltestserver-2.azurewebsites.net/donationsSavings';
+const donationSavingsURL = 'https://trinitycapitaltestserver-2.azurewebsites.net/donationsSavings';
 
 // Store the received profiles in a global variable or a state variable if you're using a front-end framework
 let Profiles = [];
