6cdb7d3df07ff3730874253969c24cb8b211f630
Jakeagle <114176194+Jakeagle@users.noreply.github.com>
2024-12-30 10:59:04 -0600

    Update script.js

--- Frontend/Javascript/script.js
+++ Frontend/Javascript/script.js
@@ -384,12 +384,7 @@ const loginFunc = function (PIN, user, screen) {
     mainApp.style.display = 'flex';
     mainApp.style.opacity = 100;
 
-    if (
-      currentProfile.memberName === 'Darlene Jones' ||
-      currentProfile.memberName === 'Jakob Ferguson'
-    ) {
-      startTimers();
-    }
+   
 
     const lessonName = document.querySelector('.lessonHeaderText');
 
