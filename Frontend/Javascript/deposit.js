465f8572b8c12cd2da5f4ad0a7bb50f4d4a40ae8
Jake Ferguson <j.man.37.x2x@gmail.com>
2025-09-09 15:58:06 -0500

    Frontend changes

--- Frontend/Javascript/deposit.js
+++ Frontend/Javascript/deposit.js
@@ -34,7 +34,7 @@ const backBTN = $('.backBtn');
 
 const loginBTN = document.querySelector('.login__btn');
 
-const depositLink = 'https://tcstudentserver-production.up.railway.app/deposits';
+const depositLink = 'http://localhost:3000/deposits';
 
 mainApp.css('display', 'none');
 
--- Frontend/Javascript/transfer.js
+++ Frontend/Javascript/transfer.js
@@ -36,7 +36,7 @@ let accountRecieve;
 
 let amount;
 
-const transferLink = `https://tcstudentserver-production.up.railway.app/transfer`;
+const transferLink = `http://localhost:3000/transfer`;
 
 /************************************************Functions*************************************************/
 
