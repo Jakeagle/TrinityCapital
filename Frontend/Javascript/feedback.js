a1b36e5e4579d993a6cfe78c5d43969b037add3d
Jake Ferguson <j.man.37.x2x@gmail.com>
2025-09-15 12:26:16 -0500

    FRONTEND: feat: Deploy to production

--- Frontend/Javascript/feedback.js
+++ Frontend/Javascript/feedback.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const API_BASE_URL = 'http://localhost:3000';
+  const API_BASE_URL = 'https://tcstudentserver-production.up.railway.app';
 
   // --- Element Selections ---
   const feedbackDialog = document.getElementById('feedbackDialog');
