7b41f8dd550ab552e20c231c4f628ad031e76df0
Jake Ferguson <j.man.37.x2x@gmail.com>
2026-02-05 13:14:54 -0600

    FRONTEND: Changing links to prod links

--- Frontend/Javascript/sendMoney.js
+++ Frontend/Javascript/sendMoney.js
@@ -22,7 +22,7 @@ const recipient = document.querySelector('.recipients');
 const inputBTN = document.querySelector('.sendBtn');
 const backBTN = document.querySelector('.backBtn');
 
-const sendMoneyURL = `http://localhost:3000/sendFunds`;
+const sendMoneyURL = `https://tcstudentserver-production.up.railway.app/sendFunds`;
 let theRecipient;
 
 mainApp.style.display = 'none';
